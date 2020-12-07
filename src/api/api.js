0cb72b0a4d6886f76628b43fd818baf0b2c88c24
EmonHR <mdemon7475@gmail.com>
2020-12-07 23:47:43 +0600

    :tada: complete the project and and example.env file

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+require('dotenv').config();
+console.log(process.env.REACT_APP_TMDB_API)
 export default axios.create({
   baseURL: `https://api.themoviedb.org/3/`,
   params: {
